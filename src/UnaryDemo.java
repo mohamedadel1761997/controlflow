ba2f0d58116b1b2960c4f410f012f06dfcaba7ff
Mohamed <mohamedadel1761997>
2021-04-19 13:17:26 +0200

    we only commit java files and configrations

---
+++ src/UnaryDemo.java
@@ -0,0 +1,14 @@
+public class UnaryDemo {
+    public static void main(String[] args) {
+
+        int num=5;
+        long result=1;
+        for(int i=5;i>0;i--)
+        {
+            result=result*i;
+
+        }
+
+        System.out.println(result);
+    }
+}
